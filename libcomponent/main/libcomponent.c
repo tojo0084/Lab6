962732a9b6f8c818a40fcd36eefc720381a8b872
tojo0084 <tojo0084@gapps.umu.se>
2017-04-28 21:51:05 +0200

    Implementation och tester av libcomponent med funktionen e_resistance

--- libcomponent/main/libcomponent.c
+++ libcomponent/main/libcomponent.c
@@ -1,7 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h> // qsort
+#include <math.h> // innehåller funktionen fabs (absolutvärdet för float/double) och även pow (uppöjt till)
+//vid kompilering av math.h: använd -lm (math library is named libm.so)
+
+
+//#include <limits.h> // innehåller ULLONG_MAX
+//#include <float.h> // FLT_MIN
+//#include <tgmath.h>
+//#include <errno.h>
+//#include <fenv.h>
+
 #include "libcomponent.h"
 
 // Tomas Johansson, Bibliotek 3, libcomponent.so
+
+float FLOAT_DELTA_VALUE_FOR_SORTING = 0.000000001;
+
+int cmpfunc(const void * a, const void * b) {
+	float val1 = *(float*)a;
+	float val2 = *(float*)b;
+	float diff = val1 - val2;
+	if(fabs(diff) < FLOAT_DELTA_VALUE_FOR_SORTING) return 0;
+	if(diff < 0) return 1; // returnerar positivt tal för att sortera i minskande ordning
+	return -1;
+}
+
+
+
 int e_resistance(float orig_resistance, float *res_array) {
-	// ToDo ...
+	long double e12_values[] = { 1.00,  1.20,  1.50,  1.80 , 2.20,  2.70,  3.30 , 3.90 , 4.70, 5.60, 6.80, 8.20  };
+	int numberOfValuesPerE12sequence = sizeof(e12_values) / sizeof(long double); // istället för att hårdkdoa 12
+	int maxMinusPower = 7; // 1^-7 dvs ett upphöjt till -7
+	int maxPlusPower = 7; // 1^7
+	int arrayIndex = 0;
+	int numberOfArrayItems = numberOfValuesPerE12sequence * (maxMinusPower + maxPlusPower + 1);
+	long double values[numberOfArrayItems];
+
+	long double powerFactorDouble;
+
+	for(int i=-maxMinusPower; i<=maxPlusPower; i++) {
+		powerFactorDouble = pow(10, i);
+		for(int j=0; j<12; j++) {
+			long double value = powerFactorDouble * e12_values[j];
+			values[arrayIndex] = value;
+			arrayIndex++;
+		}
+	}
+	for(int i=0; i<numberOfArrayItems; i++) {
+		// printf("e_resistance arr i : %Lg \n", values[i]);
+	}
+	
+	long double delta = 0.00001;
+	long double sum_ij;
+	
+	long double sum_ijk;
+	int loopCounter = 0;
+	// syftet med yttersta while-loopen är att företrädesvis välja endast ett värde, och i andra hand två värden, och som sista utväg tre värden
+	// därför loopas först alla värden för att se om de ensamma kan summer till önskat värde,
+	// sedan summeras alla kombinationer av två värden, och sist alla kombinationer av tre värden
+	while(loopCounter < 3) {
+		loopCounter++;
+		for(int i=0; i<numberOfArrayItems-2; i++) {
+			if(fabs(orig_resistance-values[i]) < delta) {
+				res_array[0] = values[i];
+				return 1;
+			}
+			if(loopCounter == 1) continue;
+			for(int j=i+1; j<numberOfArrayItems-1; j++) {
+				sum_ij = values[i] + values[j];
+				if(fabs(orig_resistance-sum_ij) < delta) {
+					res_array[0] = values[i];
+					res_array[1] = values[j];
+					qsort(res_array, 2, sizeof(float), cmpfunc);
+					return 2;
+				}
+				if(loopCounter == 2) continue;
+				for(int k=j+1; k<numberOfArrayItems; k++) {
+					sum_ijk = values[i] + values[j] + values[k];
+					if(fabs(orig_resistance-sum_ijk) < delta) {
+						res_array[0] = values[i];
+						res_array[1] = values[j];
+						res_array[2] = values[k];
+						qsort(res_array, 3, sizeof(float), cmpfunc);
+						return 3;
+					}
+				}
+			}
+		}
+	}
 	return 0;
 }
--- libcomponent/test/test.c
+++ libcomponent/test/test.c
@@ -1,27 +1,82 @@
-// gcc test.c unity/unity.c ../main/libcomponent.c -o test
+//gcc test.c unity/unity.c ../main/libcomponent.c -lm -o test
+// -lm är för att kompilera math.h (math library is named libm.so)
 
 #include "unity/unity.h"
 #include "../main/libcomponent.h"
 
-void test_e_resistance(void)
-{
+void test_e_resistance(void) {
+	float delta = 0.00001;
 	float res_array[3];
-	int count = e_resistance(1398, res_array);
+	int count;
 
-	// ToDo: Aktivera bortkommenterade raderna nedan när funtionen är implementerad
-	// TEST_ASSERT_EQUAL_INT(3, count);
+	count = e_resistance(1200, res_array);
+	TEST_ASSERT_EQUAL_INT(1, count);
+	TEST_ASSERT_FLOAT_WITHIN(delta, 1200, res_array[0]);
 
-	float delta = 0.001;
-	// TEST_ASSERT_FLOAT_WITHIN(delta, 1200, res_array[0]);
-	// TEST_ASSERT_FLOAT_WITHIN(delta, 180, res_array[1]);
-	// TEST_ASSERT_FLOAT_WITHIN(delta, 18, res_array[2])
+
+	count = e_resistance(1380, res_array);
+	TEST_ASSERT_EQUAL_INT(2, count);
+	TEST_ASSERT_FLOAT_WITHIN(delta, 1200, res_array[0]);
+	TEST_ASSERT_FLOAT_WITHIN(delta, 180, res_array[1]);
+
+
+	count = e_resistance(1398, res_array);
+	TEST_ASSERT_EQUAL_INT(3, count);
+	TEST_ASSERT_FLOAT_WITHIN(delta, 1200, res_array[0]);
+	TEST_ASSERT_FLOAT_WITHIN(delta, 180, res_array[1]);
+	TEST_ASSERT_FLOAT_WITHIN(delta, 18, res_array[2]);
+
 }
 
-int main(void)
-{
+void test_e_resistance_2(void) {
+	float delta = 0.00001;
+	float res_array[3];
+	int count;
+
+	count = e_resistance(4.70, res_array);
+	
+
+	// float e12_values[] = { 1.00,  1.20,  1.50,  1.80 , 2.20,  2.70,  3.30 , 3.90 , 4.70, 5.60, 6.80, 8.20  };
+	
+	count = e_resistance(4.70, res_array);
+	TEST_ASSERT_EQUAL_INT(1, count);
+	TEST_ASSERT_FLOAT_WITHIN(delta, 4.70, res_array[0]);
+/* */
+	count = e_resistance(6.80, res_array);
+	TEST_ASSERT_EQUAL_INT(1, count);
+	TEST_ASSERT_FLOAT_WITHIN(delta, 6.80, res_array[0]);
+
+	count = e_resistance(8.30, res_array);
+	TEST_ASSERT_EQUAL_INT(2, count);
+	TEST_ASSERT_FLOAT_WITHIN(delta, 8.20, res_array[0]);
+	TEST_ASSERT_FLOAT_WITHIN(delta, 0.10, res_array[1]);
+
+	count = e_resistance(8.60, res_array);
+	TEST_ASSERT_EQUAL_INT(2, count);
+	TEST_ASSERT_FLOAT_WITHIN(delta, 6.8, res_array[0]);
+	TEST_ASSERT_FLOAT_WITHIN(delta, 1.8, res_array[1]);
+
+
+	// OBS! värdet 6.1 är ett ganska bra exempel.
+	// Dels fungerar inte den enklaste algoritmen jag först tänkte mig dvs att hitta det största värdet och sedan fylla på med mindre ...
+	// och dessutom kan den ge både tre eller två värden som resultat och då väljer man rimligen det lägsta
+	count = e_resistance(6.10, res_array);
+//	TEST_ASSERT_EQUAL_INT(3, count);
+//	TEST_ASSERT_FLOAT_WITHIN(delta, 3.3, res_array[0]);
+//	TEST_ASSERT_FLOAT_WITHIN(delta, 2.7, res_array[1]);
+//	TEST_ASSERT_FLOAT_WITHIN(delta, 0.1, res_array[2]);
+// ovanstående är ett alternativ men den nedan ger färre
+	TEST_ASSERT_EQUAL_INT(2, count);
+	TEST_ASSERT_FLOAT_WITHIN(delta, 3.9, res_array[0]);
+	TEST_ASSERT_FLOAT_WITHIN(delta, 2.2, res_array[1]);
+}
+
+
+int main(void) {
 	UnityBegin("test/test.c");
 
 	RUN_TEST(test_e_resistance);
+	RUN_TEST(test_e_resistance_2);
 
 	UnityEnd();
 

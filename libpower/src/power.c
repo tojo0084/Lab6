ca854182d6e636c7d45501c768b718a09dc282a5
lulu0011 <lulu0011@student.umu.se>
2017-05-11 12:04:03 +0200

    c

---
+++ libpower/src/power.c
@@ -0,0 +1,27 @@
+/**
+ * power.c
+ * Ludvig Lundberg
+ * lulu0011@student.umu.se
+ * lulu0011
+ * 2017-05-05
+ */
+
+#include "power.h"
+
+/**
+ * Funktionens syfte: Beräkna effekten enligt formeln P=U^2*R.
+ * In: Spänningen samt resistansen som effekten ska beräknas utifrån.
+ * Ut: Effekten utifrån den angivna spänningen samt resistansen.
+ */
+float calc_power_r(float volt, float resistance){
+  return (volt*volt)/resistance;
+}
+
+/**
+ * Funktionens syfte: Beräkna effekten utifrån formeln P=U*I.
+ * In: Spänningen och strömmen effekten ska beräknas utifrån.
+ * Ut: Effekten utifrån den angivna spänningen samt strömmen.
+ */
+float calc_power_i(float volt, float current){
+  return volt*current;
+}
---
+++ libpower/src/power.h
@@ -0,0 +1,26 @@
+/**
+ * power.h
+ * Ludvig Lundberg
+ * lulu0011@student.umu.se
+ * lulu0011
+ * 2017-05-05
+ */
+
+#ifndef _POWER_
+#define _POWER_
+
+/**
+ * Funktionens syfte: Beräkna effekten enligt formeln P=U^2*R.
+ * In: Spänningen samt resistansen som effekten ska beräknas utifrån.
+ * Ut: Effekten utifrån den angivna spänningen samt resistansen.
+ */
+float calc_power_r(float volt, float resistance);
+
+/**
+ * Funktionens syfte: Beräkna effekten utifrån formeln P=U*I.
+ * In: Spänningen och strömmen effekten ska beräknas utifrån.
+ * Ut: Effekten utifrån den angivna spänningen samt strömmen.
+ */
+float calc_power_i(float volt, float current);
+
+#endif
